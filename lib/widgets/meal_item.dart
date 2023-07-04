2cd190f497e4df184fb116c0ad67ed3d21c1c92b
daniel-liberto <daniel.liberto@hotmail.com>
2023-07-04 20:16:42 -0300

    meals are now correct displaying on screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,13 +10,55 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      // clipBehavior força o Stack a receber o borderradius do Card, do parent dele.
+      clipBehavior: Clip.hardEdge,
+      elevation: 2, // eleveation is a shadow behind card
       child: InkWell(
         onTap: () {},
+        // Stack() is needed for put something above images.
         child: Stack(
           children: [
             FadeInImage(
-                placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(''))
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 44, vertical: 6),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
